a3892dfd786534ab543a32dd6f1ad79c76716c9c
Clemens Paumgarten <clemens@mindstream.at>
2020-04-23 15:00:30 +0200

    updated index

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,17 @@ import styled from 'styled-components'
 import 'styles/global.scss'
 
 const Container = styled.div`
-  display: flex;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
 `
 
 const Application: FunctionComponent = () => (
-  <Container>
-    <span>
-      Frontend project boilerplate with&nbsp;
-      <a href='https://reactjs.org/' target='_blank'>react</a>, <a href='https://www.styled-components.com/' target='_blank'>styled-components</a> and <a href='https://webpack.js.org/' target='_blank'>webpack</a>
-    </span>
-  </Container>
+    <Container>
+        <span>Frontend Boilerplate</span>
+    </Container>
 )
 
 render(<Application />, document.getElementById('mount'))
