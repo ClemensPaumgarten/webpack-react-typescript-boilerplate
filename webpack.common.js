c381a9d7332a8bcd745ca33dff780cd17b6e7f4f
Clemens Paumgarten <clemens@mindstream.at>
2020-04-23 14:50:42 +0200

    updated packages and webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -5,111 +5,114 @@ const ForkTypeScriptCheckerWebpackPlugin = require('fork-ts-checker-webpack-plug
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index'),
-  output: {
-    filename: 'bundle.[chunkhash:4].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-  },
+    entry: path.resolve(__dirname, 'src/index'),
+    output: {
+        filename: 'bundle.[chunkhash:4].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              plugins: ['babel-plugin-styled-components'],
-              presets: [
-                '@babel/preset-react',
-                '@babel/preset-typescript',
-                [
-                  '@babel/preset-env',
-                  {
-                    useBuiltIns: 'usage',
-                    corejs: 3,
-                    targets: 'ie 11, not dead, safari > 9',
-                  },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    // 'eslint-loader',
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            plugins: [
+                                '@babel/plugin-transform-runtime',
+                                'babel-plugin-styled-components',
+                            ],
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        useBuiltIns: 'usage',
+                                        corejs: 3,
+                                        targets: 'ie 11, not dead, safari > 9',
+                                    },
+                                ],
+                                '@babel/preset-react',
+                                '@babel/preset-typescript',
+                            ],
+                        },
+                    },
                 ],
-              ],
             },
-          },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            dimensions: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg|)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '../',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    },
+                ],
+            },
         ],
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              dimensions: true,
-              replaceAttrValues: {
-                '#ff00ff': 'currentColor',
-                '#f0f': 'currentColor',
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpg|)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-          },
-        }]
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it uses publicPath in webpackOptions.output
-              publicPath: '../',
-              hmr: process.env.NODE_ENV === 'development',
-            },
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
-        ],
-      },
+    },
+
+    plugins: [
+        new ForkTypeScriptCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'assets/index.html'),
+            minify: true,
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Ena
+        }),
     ],
-  },
 
-  plugins: [
-    new ForkTypeScriptCheckerWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'assets/index.html'),
-      minify: true,
-    }),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-      ignoreOrder: false, // Ena
-    }),
-  ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        plugins: [new TsconfigPathsPlugin()],
+    },
 
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    plugins: [new TsconfigPathsPlugin()],
-  },
+    stats: {
+        children: false,
+        chunks: false,
+        modules: false,
+        exclude: ['node_modules'],
+    },
 
-  stats: {
-    children: false,
-    chunks: false,
-    modules: false,
-    exclude: ['node_modules'],
-  },
-
-  cache: true,
+    cache: true,
 }
